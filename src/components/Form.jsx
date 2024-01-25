e1d9d675f7f3008d30b2812fa98ace04a1f2ee0c
HLena <gordillo.vi20@gmail.com>
2024-01-25 16:02:19 -0500

    Implement functions to handle onChange and onSubmit events on the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,36 @@
-import { Form as FormBt, Row, Col } from "react-bootstrap"
+import { Form as FormBt, Row, Col, Button, Alert } from "react-bootstrap"
 import useCategories from "../hooks/useCategories"
+import { useState } from "react";
 
 const Form = () => {
 
+    const [search, setSearch] = useState({
+        name: '',
+        category: ''
+    });
+    const [alert, setAlert] = useState('');
+
     const { categories } = useCategories();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(Object.values(search).includes('')) {
+            setAlert('All fields are required')
+            return;
+        }
+        setAlert('');
+
+    }
+
     return (
-        <FormBt>
+        <FormBt
+            onSubmit={ handleSubmit }
+        >
+            {
+                alert && 
+                    <Alert  variant="danger" className="text-center">{alert}</Alert>
+            }
             <Row>
                 <Col>
                     <FormBt.Group>
@@ -15,12 +40,25 @@ const Form = () => {
                             type="text"
                             placeholder="Ex: Tequila,  wine, coffee"
                             name="name"
+                            value= {search.name}
+                            onChange={ (e) => setSearch({
+                                ...search,
+                                [e.target.name] : e.target.value
+                            })}
                         />
                     </FormBt.Group>
                 </Col>
                 <Col>
-                    <FormBt.Label  htmlFor="name">Drink name</FormBt.Label>
-                    <FormBt.Select>
+                    <FormBt.Label  htmlFor="category">Drink name</FormBt.Label>
+                    <FormBt.Select
+                        id="category"
+                        name="category"
+                        value= {search.category}
+                        onChange={ (e) => setSearch({
+                            ...search,
+                            [e.target.name] : e.target.value
+                        })}
+                    >
                         <option value="">Select category</option>
                         {
                             categories.map(category => (
@@ -33,7 +71,17 @@ const Form = () => {
                             ))
                         }
                     </FormBt.Select>
+                </Col>
+            </Row>
 
+            <Row className="justify-content-end py-3">
+                <Col md={3}>
+                    <Button 
+                        type="submit"
+                        variant="danger"
+                        className="text-uppercase w-100">
+                        Search Drinks
+                    </Button>
                 </Col>
             </Row>
         </FormBt>
