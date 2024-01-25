2f0b3c235aaabbe0af33c753c31749ca3fe5bc6a
HLena <gordillo.vi20@gmail.com>
2024-01-25 14:23:18 -0500

    Implement "CategoriesProvider" in App.jsx to manage the global state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { Container } from "react-bootstrap"
 import Form from "./components/Form"
+import { CategoriesProvider } from './context/CategoriesProvider'
 
 function App() {
 
   return (
-    <>
+    <CategoriesProvider>
       <header className="py-5">
         <h1> Drink Finder</h1>
       </header>
       <Container className="py-5">
         <Form/>
       </Container>
-    </>
+    </CategoriesProvider>
   )
 }
 
