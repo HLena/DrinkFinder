cf70f852a1ff0b8af45d32a7f9034c03f882442e
HLena <gordillo.vi20@gmail.com>
2024-01-25 14:19:02 -0500

    Create a category context component

---
+++ src/context/CategoriesProvider.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react"
+
+const CategoriesContext = createContext();
+
+
+const CategoriesProvider = ({children}) => {
+
+
+
+  return (
+    <CategoriesContext.Provider 
+      value={{
+      }}
+    >
+      {children}
+    </CategoriesContext.Provider>
+  )
+}
+
+
+export {
+  CategoriesProvider
+}
+
+export default CategoriesContext
