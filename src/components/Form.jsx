4de7b0882db0fa1ba50785ea1e3edf9b0eabc158
HLena <gordillo.vi20@gmail.com>
2024-01-25 14:38:38 -0500

    Use "useCategories" hook to get the list of categories

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,43 @@
 import { Form as FormBt, Row, Col } from "react-bootstrap"
+import useCategories from "../hooks/useCategories"
 
 const Form = () => {
-  return (
-    <FormBt>
-        <Row>
-            <Col>
-                <FormBt.Group>
-                    <FormBt.Label  htmlFor="name">Drink name</FormBt.Label>
-                    <FormBt.Control 
-                        id="name"
-                        type="text"
-                        placeholder="Ex: Tequila,  wine, coffee"
-                        name="name"
-                    />
-                </FormBt.Group>
-            </Col>
-            <Col>
-                <FormBt.Label  htmlFor="name">Drink name</FormBt.Label>
-                <FormBt.Select>
-                    <option value="">Select category</option>
-                </FormBt.Select>
 
-            </Col>
-        </Row>
-    </FormBt>
-  )
+    const { categories } = useCategories();
+    return (
+        <FormBt>
+            <Row>
+                <Col>
+                    <FormBt.Group>
+                        <FormBt.Label  htmlFor="name">Drink name</FormBt.Label>
+                        <FormBt.Control 
+                            id="name"
+                            type="text"
+                            placeholder="Ex: Tequila,  wine, coffee"
+                            name="name"
+                        />
+                    </FormBt.Group>
+                </Col>
+                <Col>
+                    <FormBt.Label  htmlFor="name">Drink name</FormBt.Label>
+                    <FormBt.Select>
+                        <option value="">Select category</option>
+                        {
+                            categories.map(category => (
+                                <option 
+                                    key={category.strCategory}
+                                    value={category.strCategory}
+                                >
+                                    {category.strCategory}
+                                </option>
+                            ))
+                        }
+                    </FormBt.Select>
+
+                </Col>
+            </Row>
+        </FormBt>
+    )
 }
 
 export default Form
